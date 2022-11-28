9f373ab7b1262520eacc2df10bc7462f044e4671
shakhlevich <qa.shakhlevich@gmail.com>
2022-11-28 11:47:57 +0300

    change

--- H_W_4/task_15.ts
+++ H_W_4/task_15.ts
@@ -7,10 +7,30 @@
 const readline = require("readline-sync");
 const strat = Date.now()
 const result:string= readline.question("Type something: ");
-const sumNum  = parseFloat(result.replace(/[^\d]/g, '')) // НЕ НАШЁЛ КАК ПОЛУЧИТЬ СУММУ
+
+//const sumNum  = parseFloat(result.replace(/[^\d]/g, '')) 
+let sumNum  = 0;
+let regexResult: (string | number)[] | null;
+const regexp = /\d/g;
+
+while(regexResult = regexp.exec(result)) sumNum += +regexResult[0];
+
+result.split('').reduce((accum, current) => accum + (isNaN(+current) ? 0 : +current) , 0)
+
+for (const letter of result) {
+    if(!isNaN(+letter)) sumNum += +letter
+}
+
+
+
+const mm = `${Date.now() - strat}`
+const mmMS = (+mm)/1000
+
+
+
+
 
 console.log(sumNum)
-console.log(`${Date.now() - strat}ms`) // НЕ НАШЁЛ КАК МИЛЛИСЕКУНДЫ  ПЕРЕВЕСТИ В СЕКУНДЫ
-
+console.log(`${mmMS}sec`)
 
 
