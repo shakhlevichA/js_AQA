2f44a4796ba26099bc0d0a303cd8f72c32f10202
shakhlevichA <110105192+shakhlevichA@users.noreply.github.com>
2022-11-28 11:54:07 +0300

    Update task_15.ts

--- H_W_4/task_15.ts
+++ H_W_4/task_15.ts
@@ -3,14 +3,23 @@
 //(с математическим округлением) и сумму всех цифр в тексте
  
 
-
 const readline = require("readline-sync");
 const strat = Date.now()
 const result:string= readline.question("Type something: ");
-const sumNum  = parseFloat(result.replace(/[^\d]/g, '')) // НЕ НАШЁЛ КАК ПОЛУЧИТЬ СУММУ
 
-console.log(sumNum)
-console.log(`${Date.now() - strat}ms`) // НЕ НАШЁЛ КАК МИЛЛИСЕКУНДЫ  ПЕРЕВЕСТИ В СЕКУНДЫ
+let sumNum  = 0;
+let regexResult: (string | number)[] | null;
+const regexp = /\d/g;
+
+while(regexResult = regexp.exec(result)) sumNum += +regexResult[0];
+
+result.split('').reduce((accum, current) => accum + (isNaN(+current) ? 0 : +current) , 0)
+
+for (const letter of result) {
+    if(!isNaN(+letter)) sumNum += +letter
+}
 
 
 
+const mm = `${Date.now() - strat}`
+const mmMS = (+mm)/1000
