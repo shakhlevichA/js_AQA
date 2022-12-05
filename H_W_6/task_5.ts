c3d292fa24ffd5f0af2c2f32f43677f7f98783ae
shakhlevichA <110105192+shakhlevichA@users.noreply.github.com>
2022-12-05 12:42:45 +0300

    Update task_5.ts

--- H_W_6/task_5.ts
+++ H_W_6/task_5.ts
@@ -12,7 +12,6 @@
 // Вычислите среднюю зарплату сотрудников 
 //и результат поместите в соответствующую переменную
 
-
 let salaries = {
         andrey: 500,
         sveta: 413,
@@ -23,8 +22,21 @@ let salaries = {
     let avergeSalary: number;
     let salariesSum = 0;
     for(const Key in salaries){
-       
-    salariesSum += ((salaries as any)[Key]) / Object.keys(salaries).length
-      
-    }
+       salariesSum += ((salaries as any)[Key]) 
+      }
+    salariesSum /= Object.keys(salaries).length
     console.log(salariesSum)
+    
+
+    const iterator = {};
+
+//iterator[Symbol.iterator] = function () 
+ for (const iterator of salaries) {
+    salariesSum += ((salaries as any)[iterator])
+}
+
+// for or просит создать метод
+//(Тип "{ andrey: number; sveta: number; anton: number; Andrey: number; alexandra: number; }" должен иметь метод "[Symbol.iterator]()", 
+//который возвращает итератор.)
+
+//КАК ОНО БУДЕТ ВЫГЛЯДИТЬ ?
