eca9e5cc7f9d9226c7fdb51feef9ef37be4b0ed8
shakhlevich <qa.shakhlevich@gmail.com>
2022-12-26 19:01:30 +0300

    Introduction to Test Automation

---
+++ H_W_17/task_5.ts
@@ -0,0 +1,7 @@
+// Для Одиночных тегов: сделать инпуты radio или с видами, перечисленными на слайде.
+// Группа выглядит следующим образом:
+//   <fieldset id="group1">
+//     <input type="radio" value="value1" name="group1">
+//     <input type="radio" value="value2" name="group1">
+//   </fieldset>
+  
